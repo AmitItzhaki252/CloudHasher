d00c909274b0d4913fcb45baa78247ad5ac1996e
Amit <88612214+AmitItzhaki252@users.noreply.github.com>
2023-06-12 17:55:21 +0300

    .

--- StartScripts/InstallEndpoint.sh
+++ StartScripts/InstallEndpoint.sh
@@ -1,9 +1,4 @@
 #!/home/ubuntu
-
-git clone https://github.com/AmitItzhaki252/CloudHasher.git
-
-cd CloudHasher/Endpoint
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
@@ -13,12 +8,14 @@ sudo pip3 install --upgrade pip
 
 sudo pip3 install flask
 
-sudo pip3 install flasgger
-sudo pip3 install paramiko
-sudo pip3 install requests
+python3 -c "import flask"
 
-sudo pip3 install flask boto3 pillow
+sudo pip3 install flask boto3 pillow flasgger paramiko requests
 
 sudo apt install python3-flask -y
+
+git clone https://github.com/AmitItzhaki252/CloudHasher.git
+
+cd CloudHasher/Endpoint
 
 sudo python3 -m flask run --host=0.0.0.0
