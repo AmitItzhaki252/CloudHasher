68d86c630e0ef2801d9ece57c44997e1fa5ec224
Amit <88612214+AmitItzhaki252@users.noreply.github.com>
2023-06-13 17:34:22 +0300

    boto3

--- Endpoint/InstallWorker.sh
+++ Endpoint/InstallWorker.sh
@@ -11,12 +11,15 @@ sudo pip3 install flask
 
 sudo pip3 install paramiko
 sudo pip3 install requests
+sudo pip3 install boto3
 
 python3 -c "import flask"
 
 sudo pip3 install flask boto3 pillow
 
 sudo apt install python3-flask -y
+
+python3 -c "import boto3"
 
 git clone https://github.com/AmitItzhaki252/CloudHasher.git
 
--- Worker/InstallWorker.sh
+++ Worker/InstallWorker.sh
@@ -11,6 +11,7 @@ sudo pip3 install flask
 
 sudo pip3 install paramiko
 sudo pip3 install requests
+sudo pip3 install boto3
 
 python3 -c "import flask"
 
@@ -18,8 +19,10 @@ sudo pip3 install flask boto3 pillow
 
 sudo apt install python3-flask -y
 
+python3 -c "import boto3"
+
 git clone https://github.com/AmitItzhaki252/CloudHasher.git
 
 cd CloudHasher/Worker
 
-sudo nohup python3 -m flask run --host=0.0.0.0 > log.txt 2>&1 &
+sudo nohup python3 -m app.py > log.txt 2>&1 &
